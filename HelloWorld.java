6655b0bd36d2ae844e340c6e1553653348dc28f8
Syentika Dey <syentika.dey12ss@gmail.com>
2026-03-11 15:11:16 +0530

    UC1: Display Hello World on console

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+// UC1 - Display Hello World
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        // Display message in console
+        System.out.println("Hello World");
+
+    }
+}
